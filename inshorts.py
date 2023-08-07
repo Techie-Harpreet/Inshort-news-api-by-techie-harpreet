b6b86164a63c0a3df3eeb7822e030346cea430d4
Techie Harpreet <93361317+Techie-Harpreet@users.noreply.github.com>
2023-08-07 12:12:53 +0530

    Update inshorts.py

--- inshorts.py
+++ inshorts.py
@@ -83,7 +83,6 @@ def getNews(category):
 
 
         newsObject = {
-                # 'id': uuid.uuid4().hex,
                 'title': title,
                 'imageUrl': imageUrl,
                 'url': readMoreUrl,
@@ -98,5 +97,103 @@ def getNews(category):
         newsDictionary['data'].append(newsObject)
 
     return(newsDictionary)
+
+    
+
+def getHindiNews(category):
+
+    newsDictionaryHindi = {
+
+    'success': True,
+        'category': category,
+        'data': []
+
+    }
+
+
+    try:
+        if category != 'all':
+            htmlBody = requests.get(
+                'https://www.inshorts.com/hi/read/' + category)
+        else:
+            htmlBody = requests.get('https://www.inshorts.com/hi/read/')
+
+    except requests.exceptions.RequestException as e:
+        newsDictionaryHindi['success'] = False
+        newsDictionaryHindi['error'] = str(e.message)
+        return newsDictionaryHindi
+    
+    soup = BeautifulSoup(htmlBody.content, 'html.parser')
+    elements = soup.findAll('div', class_='news-card z-depth-1')
+
+
+    for i in elements:
+        
+        try:
+            title = i.find('div', class_='news-card-title news-right-box').find('span').text
+            print(title)
+        except AttributeError:
+            author = None
+
+        try:
+            author = i.find('div', class_='news-card-title news-right-box').find('span',class_="author").text
+            print(author)
+        except AttributeError:
+            author = None
+
+        try:
+            time = i.find('div', class_='news-card-title news-right-box').find('span',class_="time").text
+            print(time)
+        except AttributeError:
+            time = None
+
+        try:
+            date = i.find('div', class_='news-card-title news-right-box').find('span',clas="date").text
+            print(date)
+
+        except AttributeError:
+            date = None
+
+        try:
+            content = i.find('div' , class_="news-card-content news-right-box").find('div').text
+            print(content)
+        
+        except AttributeError:
+            content = None
+        
+
+        try:
+            readMoreUrl = i.find(class_='read-more').find('a').get('href')
+            print(readMoreUrl)
+        except AttributeError:
+            readMoreUrl = None
+
+        
+        try:
+            imageUrl = i.find(class_='news-card-image')['style'].split("'")[1]
+        except AttributeError:
+                imageUrl = None
+
+        print("\n")
+
+
+
+
+
+        newsObject = {
+                'title': title,
+                'imageUrl': imageUrl,
+                'url': readMoreUrl,
+                'content': content,
+                'author': author,
+                'date': date,
+                'time': time,
+                'readMoreUrl': readMoreUrl
+            }
+        
+
+        newsDictionaryHindi['data'].append(newsObject)
+
+    return(newsDictionaryHindi)
 
     
